aab7286668d88a1b1661f1a9e00583576734e6cb
github username <github email address>
2018-10-03 14:54:25 -0400

    good commit

--- lib/a_broken_program.rb
+++ lib/a_broken_program.rb
@@ -1,1 +1,23 @@
-puts "Hello World!"
+# What is a Program?
+
+puts "This is a program"
+puts "Programs are interpreted at runtime."
+puts "Which means even though I'm writing this Tue Jan 17 2012 8:36am"
+puts "the time now is "+ Time.now.to_s
+
+puts "Programs have flow."
+puts "Which means that you see this first"
+puts "and this next."
+
+puts "Lines of code are executed linearly, that's flow."
+puts "First come, first served."
+
+puts "Programs are composed of basically three things:"
+
+puts "A language's keywords, like 'if' or 'end' (approximately 43)."
+puts "Literal pieces of data like this very sentence (or String)."
+puts "Finally, barewords, or variables, that are set equal to things."
+
+puts "Anything that isn't one of those will cause an error."
+
+see
