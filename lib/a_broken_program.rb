f6a793a9a7e505ad79e6e16d3eae9bc8b106cabf
mkay0961 <mkay0961@gmail.com>
2018-12-20 12:02:57 -0500

    done

--- lib/a_broken_program.rb
+++ lib/a_broken_program.rb
@@ -18,6 +18,6 @@ puts "A language's keywords, like 'if' or 'end' (approximately 43)."
 puts "Literal pieces of data like this very sentence (or String)."
 puts "Finally, barewords, or variables, that are set equal to things."
 
-puts "Anything that isn't one of those will cause an error."
+puts "Anything that isn't one of those will cause an error.
 
 see
