f1664d9229ffc0cb1aca4c06ab4198f6519079ce
rmaghsoudi <romy_mag@outlook.com>
2019-02-21 17:39:51 -0500

    Done.

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -2,4 +2,3 @@ puts "Hello World!" # Correct this to Hello World!
 
 # Your entire program should read:
 # puts Hello World!
-# sneaky with the quotation marks, I almost panicked when the whole block changed colors
